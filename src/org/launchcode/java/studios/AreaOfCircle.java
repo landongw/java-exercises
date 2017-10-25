bbf75b903597a3870961bfa1e06910717a4029c0
landongw <landon@extantpublishing.com>
2017-10-25 14:37:43 -0500

    Add errors for negative numbers and zero, loop input until no errors

--- src/org/launchcode/java/studios/AreaOfCircle.java
+++ src/org/launchcode/java/studios/AreaOfCircle.java
@@ -5,19 +5,30 @@ import java.util.Scanner;
 public class AreaOfCircle {
     public static void main(String[] args) {
         double pi = 3.14;
-        float r;
+        float r = 0;
         float A;
+        boolean rB = true;  // true means r has not been initialized
 
-        Scanner in1;
-        in1 = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle: ");
-        r = in1.nextFloat();
+//        if (r == null) {
+//            rB = false;
+//        }
 
-        if (r < 0) {
-            System.out.println("Error: negative number not make circle.");
-        } else {
-            A = (float) pi * r * r;
-            System.out.println("The area of the circle of radius " + r + " is " + A + ".");
+        while (rB) {
+            Scanner in1;
+            in1 = new Scanner(System.in);
+            System.out.println("Enter the radius of the circle: ");
+            r = in1.nextFloat();
+            if (r < 0) {
+                System.out.println("Error: negative number not make circle.");
+            } else if (r == 0) {
+                System.out.println("Error: zero not make circle.");
+            } else {
+                rB = false;
+            }
         }
+
+        A = (float) pi * r * r;
+        System.out.println("The area of the circle of radius " + r + " is " + A + ".");
+
     }
 }
